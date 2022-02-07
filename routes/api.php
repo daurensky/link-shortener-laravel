e4ac1d57f05e710373aced3e2456eab3333ef3fe
Dauren Kambarov <dkambarov17@gmail.com>
2022-02-07 22:10:36 +0600

    Remove test route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,4 @@
 use App\Http\Controllers\Api\LinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return \App\Models\Link::all();
-});
 Route::post('store', [LinkController::class, 'store']);
